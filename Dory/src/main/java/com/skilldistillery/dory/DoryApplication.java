5348b2535633db274576f05e8a936ccacf3dd9a4
Jay-tray <113144309+Jay-tray@users.noreply.github.com>
2023-01-20 16:30:55 -0700

    Create Boot/Rest project, add JPA dependency

---
+++ Dory/src/main/java/com/skilldistillery/dory/DoryApplication.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.dory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DoryApplication.class, args);
+	}
+
+}
---
+++ Dory/src/test/java/com/skilldistillery/dory/DoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.dory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
